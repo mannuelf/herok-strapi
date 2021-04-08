d06059ecdbbcf51d7c150c977258772464525097
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-04-08 09:41:41 +0200

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '31d11e38ff85e308897dba1e2816511c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3f82485-93f6-4caf-84fc-4e50ee842dc1'
+};
